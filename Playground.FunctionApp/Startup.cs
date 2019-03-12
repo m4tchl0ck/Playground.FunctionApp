500d080d3fd73dea9c288ead5bb925ebf19b00a8
Adrian Rusznica <adrian@rusznica.pl>
2019-03-12 15:20:29 +0100

    Add Startup where SomeService is registered

---
+++ Playground.FunctionApp/Startup.cs
@@ -0,0 +1,17 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Playground.FunctionApp;
+
+[assembly: WebJobsStartup(typeof(Startup), "Startup")]
+namespace Playground.FunctionApp
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.Services.AddSingleton<ISomeService, SomeService>();
+        }
+    }
+}
